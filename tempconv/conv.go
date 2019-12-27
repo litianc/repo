8bf2992ad4c8473833ee4dae5ff8e6c9af9205fd
litianc <719153161@qq.com>
2019-12-27 12:01:00 +0800

    solve Celsius.string reclare problem

--- tempconv/conv.go
+++ tempconv/conv.go
@@ -8,5 +8,3 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-// c.string
-func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
